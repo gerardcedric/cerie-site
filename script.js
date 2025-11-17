454abdf25519091a3adaa5f426a2bdcb0059b820
gerardcedric <sasu.cerie@gmail.com>
2025-11-17 15:26:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+// MENU MOBILE
+document.getElementById('navToggle').addEventListener('click', () => {
+  document.getElementById('nav').classList.toggle('show');
+});
+
+// CARROUSEL SWIPER
 document.addEventListener('DOMContentLoaded', () => {
   new Swiper('.realisations-swiper', {
-    loop:true,
-    slidesPerView:1,
-    spaceBetween:20,
-    pagination:{ el:'.swiper-pagination', clickable:true },
-    navigation:{ nextEl:'.swiper-button-next', prevEl:'.swiper-button-prev' },
-    breakpoints:{768:{slidesPerView:2},1100:{slidesPerView:3}}
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      1100: { slidesPerView: 3 }
+    }
   });
-  GLightbox({ selector: '.glightbox' });
+
+  // LIGHTBOX
+  GLightbox({
+    selector: '.glightbox'
+  });
 });
